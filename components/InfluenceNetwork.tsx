e6b81d03d9aeb886a954e7944d8fe54ef981a7ce
Yasuhiro Sugawara <yasu@sonicgarden.jp>
2024-04-05 16:32:32 +0900

    add motion

--- components/InfluenceNetwork.tsx
+++ components/InfluenceNetwork.tsx
@@ -133,28 +133,51 @@ const NetworkGraph: React.FC<NetworkGraphProps> = ({ nodes, links }) => {
     .enter()
     .append("circle")
       .attr("r", 20)
-        .style("fill", "#69b3a2");
+        .style("fill", "#69b3a2")        .call(d3.drag()
+          .on('start', dragstarted)
+          .on('drag', dragged)
+          .on('end', dragended)
+        );;
+           
+      node.append('title')
+        .text((d: any) => d.name);      
       
       const simulation = d3.forceSimulation(nodes)                 // Force algorithm is applied to data.nodes
-      .force("link", d3.forceLink()                            // This force provides links between nodes
-            .id(function(d) { return d.id; })                     // This provide  the id of a node
-            .links(links)                                    // and this the list of links
-      )
-      .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
-      .force("center", d3.forceCenter(width / 2, height / 2))     // This force attracts nodes to the center of the svg area
-      .on("end", ticked);
+        .force("link", d3.forceLink()                            // This force provides links between nodes
+          .id(function (d) { return d.id; })                     // This provide  the id of a node
+          .links(links)                                    // and this the list of links
+        )
+        .force("charge", d3.forceManyBody().strength(-400))         // This adds repulsion between nodes. Play with the -400 for the repulsion strength
+        .force("center", d3.forceCenter(width / 2, height / 2));     // This force attracts nodes to the center of the svg area
 
-      function ticked() {
+      simulation.on('tick', () => {
         link
-            .attr("x1", function(d) { return d.source.x; })
-            .attr("y1", function(d) { return d.source.y; })
-            .attr("x2", function(d) { return d.target.x; })
-            .attr("y2", function(d) { return d.target.y; });
+          .attr('x1', (d: any) => d.source.x)
+          .attr('y1', (d: any) => d.source.y)
+          .attr('x2', (d: any) => d.target.x)
+          .attr('y2', (d: any) => d.target.y);
 
         node
-            .attr("cx", function (d) { return d.x+6; })
-            .attr("cy", function(d) { return d.y-6; });
+          .attr('cx', (d: any) => d.x)
+          .attr('cy', (d: any) => d.y);
+      });      
+
+      function dragstarted(event: any, d: any) {
+        if (!event.active) simulation.alphaTarget(0.3).restart();
+        d.fx = d.x;
+        d.fy = d.y;
       }
+
+      function dragged(event: any, d: any) {
+        d.fx = event.x;
+        d.fy = event.y;
+      }
+
+      function dragended(event: any, d: any) {
+        if (!event.active) simulation.alphaTarget(0);
+        d.fx = null;
+        d.fy = null;
+      }      
     }
   }, [nodes, links]);
 
